71d4aba181d3f6aaf6abb083cf8791494d352441
ckandrew <ckandrew04@gmail.com>
2022-08-07 01:36:55 -0400

    .

---
+++ ui.py
@@ -0,0 +1,27 @@
+import math
+
+from cmu_112_graphics import *
+
+def appStarted(app):
+    pass
+
+def keyPressed(app, event):
+    pass
+
+def mousePressed(app, event):
+    pass
+
+def timerFired(app):
+    pass
+
+def redrawAll(app, canvas):
+    pass
+
+
+
+
+
+
+def main():
+  
+    runApp(1920, 1080)
